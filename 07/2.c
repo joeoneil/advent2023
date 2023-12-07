b8c55adc58eec5a7eec2e8eb2e8bc3af8c95301b
joe <apjoey914@gmail.com>
2023-12-07 01:51:38 -0500

    Day 7

---
+++ 07/2.c
@@ -0,0 +1,145 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define A 14
+#define K 13
+#define Q 12
+#define J 1
+#define T 10
+
+typedef struct {
+  char *hand;
+  int bid;
+} hand;
+
+int card_score(char c) {
+  int score;
+  if (isdigit(c)) {
+    score = c - '0';
+  } else {
+    switch (c) {
+    case 'A':
+      score = A;
+      break;
+    case 'K':
+      score = K;
+      break;
+    case 'Q':
+      score = Q;
+      break;
+    case 'J':
+      score = J;
+      break;
+    case 'T':
+      score = T;
+      break;
+    }
+  }
+  return score;
+}
+
+int score_hand(hand *h) {
+  int cards[15];
+  for (int i = 0; i < 15; i++) {
+    cards[i] = 0;
+  }
+  for (int i = 0; i < 5; i++) {
+    cards[card_score(h->hand[i])]++;
+  }
+  int two = 0, three = 0, four = 0, five = 0;
+  for (int i = 0; i < 15; i++) {
+    if (i == 1)
+      continue;
+    if (cards[i] == 2) {
+      two++;
+    } else if (cards[i] == 3) {
+      three++;
+    } else if (cards[i] == 4) {
+      four++;
+    } else if (cards[i] == 5) {
+      five++;
+    }
+  }
+  int score = 0;
+  if (cards[1] > 0) {
+    if (four) {
+      score = 25; // 5 kind
+    } else if (three) {
+      if (cards[1] == 2) {
+        score = 25; // 5 kind
+      } else {
+        score = 24; // 4 kind
+      }
+    } else if (two == 2) {
+      score = 23; // full house
+    } else if (two) {
+      if (cards[1] == 3) {
+        score = 25; // 5 kind
+      } else if (cards[1] == 2) {
+        score = 24; // 4 kind
+      } else {
+        score = 22; // 3 kind
+      }
+    } else {
+      if (cards[1] == 5 || cards[1] == 4) {
+        score = 25; // 5 kind
+      } else if (cards[1] == 3) {
+        score = 24; // 4 kind
+      } else if (cards[1] == 2) {
+        score = 22; // 3 kind
+      } else {
+        score = 20; // 2 kind
+      }
+    }
+  } else {
+    if (five) {
+      score = 25;
+    } else if (four) {
+      score = 24;
+    } else if (three && two) {
+      score = 23;
+    } else if (three) {
+      score = 22;
+    } else if (two == 2) {
+      score = 21;
+    } else if (two) {
+      score = 20;
+    } else {
+      score = 0;
+    }
+  }
+  score *= 10;
+  for (int i = 0; i < 5; i++) {
+    score *= 15;
+    score += card_score(h->hand[i]);
+  }
+  return score;
+}
+
+int hand_cmp(const void *h1, const void *h2) {
+  return score_hand((hand *)h1) - score_hand((hand *)h2);
+}
+
+int main(void) {
+  long sum = 0;
+  hand h[1024];
+  int h_count;
+  while (1) {
+    char buf[1024];
+    fgets(buf, 1024, stdin);
+    if (feof(stdin)) {
+      break;
+    }
+    char *hand_s = strtok(buf, " ");
+    hand h_s = {.hand = strdup(hand_s),
+                .bid = strtol(strtok(NULL, " "), NULL, 10)};
+    h[h_count++] = h_s;
+  }
+  qsort(h, h_count, sizeof(hand), hand_cmp);
+  for (long i = 0; i < h_count; i++) {
+    sum += h[i].bid * (i + 1);
+  }
+  printf("%ld\n", sum);
+}
--- README.md
+++ README.md
@@ -13,11 +13,14 @@ challenge in C. Each solution will be in 1.c and 2.c, and read from stdin.
 | 4   | 00:10:43 | 3496   | 42    | 00:17:12 | 1612   | 53    |
 | 5   | 00:37:19 | 4305   | 75    | 00:56:17 | 1254   | 79    |
 | 6*  | 01:06:17 | 13083  | 30    | 01:12:19 | 12484  | 34    |
+| 7*  | 01:27:56 | 9455   | 105   | 01:46:44 | 7495   | 139   |
 
 ## Asterisks
 
-Day 2 I started ~15 minutes after midnight
+Day 2: I started ~15 minutes after midnight
 
-Day 6 I started at 12:57 because I was working on homework and didn't realize the time.
+Day 6: I started at 12:57 because I was working on homework and didn't realize the time.
 I'm so sad cause this one was so easy too. I could easily have gotten a top 1000 score
 if I didn't have such a skill issue with the time.
+
+Day 7: Skill issue
